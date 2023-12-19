2497eb138c4a94b95b7c9a77f8cd161a056bbf65
Manjunath <nkmanju412@gmail.com>
2023-12-19 19:12:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "mrcloudbook-cicd-bucket"
+    bucket         = "manju-cicd-bucket"
     key            = "my-terraform-environment/main"
     region         = "ap-south-1"
-    dynamodb_table = "mrcloudbook-dynamo-db-table"
+    dynamodb_table = "manju-dynamo-db-table"
   }
 }
